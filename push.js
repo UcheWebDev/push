ad6decb779af83c1bf874155c3e251f6ac869ee0
UcheWebDev <uceewebdev@gmail.com>
2021-06-17 21:03:44 +0100

    fix

--- push.js
+++ push.js
@@ -1,25 +1,26 @@
-// var push = require('web-push');
-// // let vapidkeys = push.generateVAPIDKeys();
-// let vapidkeys = {
-//     publicKey: 'BGXw6wxKhmgFHQkeONsea1mDNH8ACLGy3zDejpdQLjM4WlPkvhfEkKec7FWg7vP8-OkzT8ijeLJql9vGb2mPjX8',
-//     privateKey: 'DPJO6E4LHQlYApvOY6pmQF21uxouWR0v-BuOjDcKWwo'
-//   };
+var push = require('web-push');
+// let vapidkeys = push.generateVAPIDKeys();
+let vapidkeys = {
+    publicKey: 'BGXw6wxKhmgFHQkeONsea1mDNH8ACLGy3zDejpdQLjM4WlPkvhfEkKec7FWg7vP8-OkzT8ijeLJql9vGb2mPjX8',
+    privateKey: 'DPJO6E4LHQlYApvOY6pmQF21uxouWR0v-BuOjDcKWwo'
+  };
 
 
-//   push.setVapidDetails('mailto:publicgist321@gmail.com',vapidkeys.publicKey, vapidkeys.privateKey)
-// let sub =
-//     {
-//         endpoint:"https://fcm.googleapis.com/fcm/send/cHWS9PBn9PE:APA91bGQAtZVJkCdakn-vCpThvYgP3bz60PzZXBWfbUhaV1TxGGaltdDnNoWCUPM4mPF2sY5XmjoDXwUHdyVkomO6-6n5K8anNFSUJ2vqCFOTMI6LQJ4bmxQ3KxToiHYRBQJAQ1koqvB",
-//     expirationTime:null,
-//     keys:{
-//         p256dh:"BGlGga58p0B5BxWsJjwdDwCgZyFiXKgu_xNKazBU12p7WM01w7Pml7y4-A0zBehNdTMoszc3SomrbYfn9NERA5s",
-//         auth:"QRq7JIYibHVN1ehsCadc3A"}}
-// ;
+  push.setVapidDetails('mailto:publicgist321@gmail.com',vapidkeys.publicKey, vapidkeys.privateKey)
+let sub =
+    {
+        endpoint:"https://fcm.googleapis.com/fcm/send/cHWS9PBn9PE:APA91bGQAtZVJkCdakn-vCpThvYgP3bz60PzZXBWfbUhaV1TxGGaltdDnNoWCUPM4mPF2sY5XmjoDXwUHdyVkomO6-6n5K8anNFSUJ2vqCFOTMI6LQJ4bmxQ3KxToiHYRBQJAQ1koqvB",
+    expirationTime:null,
+    keys:{
+        p256dh:"BGlGga58p0B5BxWsJjwdDwCgZyFiXKgu_xNKazBU12p7WM01w7Pml7y4-A0zBehNdTMoszc3SomrbYfn9NERA5s",
+        auth:"QRq7JIYibHVN1ehsCadc3A"}
+    }
+;
 
 // function testpush(){
 //     push.sendNotification(sub, 'test message')
 // }
-// testpush();
+// // testpush();
 
-const btn = document.createElement("BUTTON").addEventListener('click',testpush)
-// document.body.onload = testpush;
+// const btn = document.createElement("BUTTON").addEventListener('click',testpush)
+// document.body.onload = addElement;
